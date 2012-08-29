11b8b8ae96709679050794e5e9c8460222a5f540
Orestes Domínguez <tes.orestes@gmail.com>
2012-08-29 11:47:31 +0200

    TEST new items are added to the list

--- test/spec/lists.spec.js
+++ test/spec/lists.spec.js
@@ -12,5 +12,10 @@ describe("Single LinkedList", function() {
 		assert.equal(list.values().length, 0);
 	});
 
+	it("new items are added to the list", function() {
+		var list = new LinkedList();
+		list.add('fred');
+		assert.equal(list.values().length, 1);
+	});
 });
 
